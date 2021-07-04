d0517cbb12913cb551bff6449a961f1975b89434
Felix <ezamabusiness@gmail.com>
2021-07-04 16:14:15 -0600

    added population entry, proper date field, and date as string field

--- stats/models.py
+++ stats/models.py
@@ -27,7 +27,8 @@ class Location(models.Model):
     total_recoveries = models.IntegerField(default=-1)
     total_deaths = models.IntegerField(default=-1)
     total_confirmed = models.IntegerField(default=-1)
-    time = models.CharField(max_length=10) 
+    time_as_string = models.CharField(max_length=10, default="n/a")
+    time = models.DateField(max_length=10) 
     fatality_rate = models.DecimalField(
         default=0.00, decimal_places=2, max_digits=4)
 
@@ -35,28 +36,30 @@ class Location(models.Model):
         abstract = True
 
 
+
 class Country(Location):
     iso = models.CharField(max_length=3, editable=False, default="N/A")
     regions = models.IntegerField(default=-1)
+    pop = models.IntegerField(default=-1) #population
     def __str__(self):
-        return f"{self.name} ({self.iso}) with {self.active} active cases, {self.recoveries} recoveries, {self.deaths} deaths, {self.confirmed} confirmed and a fatality rate of {self.fatality_rate} for {self.time}"
+        return f"{self.name} ({self.iso}) on {self.time}"
 
 
 class Region(Location):
     in_country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name}', in country {self.in_country} has {self.active} active cases, {self.recoveries} recoveries, {self.deaths} deaths, {self.confirmed} confirmed and a fatality rate of {self.fatality_rate} for {self.time}"
+        return f"'{self.name}', in country {self.in_country} on {self.time}"
 
 
 class Global(Location):  # Stores the global total
     def __str__(self):
-        return f"The world has {self.active} active cases, {self.recoveries} recoveries, {self.deaths} deaths, {self.confirmed} confirmed and a fatality rate of {self.fatality_rate} for {self.time}"
+        return f"World on: {str(self.time)}"
 
 
 # should only ever be one entry in this table. Stores the date the db was last refreshed
 class Last_update(models.Model):
-    time = models.CharField(default="2019-12-31", max_length=10)
+    time = models.DateField(default=datetime.date(2019,12,31), max_length=10)
 
     def __str__(self):
         return self.time
